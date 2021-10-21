6b3145be182c82063eb6fd76bb201d2755b9323e
Heesang <szpdkss3@gmail.com>
2021-10-21 09:22:01 +0900

    1021first

---
+++ job/src/main/java/com/example/job/service/JobService.java
@@ -0,0 +1,12 @@
+package com.example.job.service;
+
+import com.example.job.dto.JobDto;
+import com.example.job.jpa.JobEntity;
+
+public interface JobService {
+
+    JobEntity createJob(JobDto jobDto);
+    Iterable<JobEntity> getAllJobs();
+    Iterable<JobEntity> getCorpAllJobs(String corpNo2);
+//    필요한 서비스 추가 예정
+}
---
+++ job/src/main/java/com/example/job/vo/RequestJobInfo.java
@@ -0,0 +1,31 @@
+package com.example.job.vo;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class RequestJobInfo {
+    @NotNull
+    private Long jobsNo;
+    private String corpNo2; //기업코드?
+    private String empNo;
+    private String jobsTitle;
+    private String jobsContext;
+    @NotNull
+    private Integer recruitNum;
+    private String favoriteLang;
+    private String jobLocation;
+    private String jobType;
+    private String jobQualify;
+    private Date applyStart;
+    private String employType;
+    private String workDetail; //업무내용
+    private Date applyEnd; //지원마감일
+    private Date intvStart1; //면접1시작일
+    private Date intvEnd1;
+    private Date intvStart2;
+    private Date intvEnd2;
+
+}
