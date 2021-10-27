3ed326edea98d5d42efb1d0e284cd0f683d6d0fc
haribowoo <85086591+hariBoWoo@users.noreply.github.com>
2021-10-27 18:29:21 +0900

    최종상태

--- hr-service/src/main/java/com/example/hrservice/controller/HrController.java
+++ hr-service/src/main/java/com/example/hrservice/controller/HrController.java
@@ -23,7 +23,7 @@ import static java.lang.Boolean.FALSE;
 
 @Slf4j
 @RestController
-@RequestMapping("/")
+@RequestMapping("/hr")
 public class HrController {
 
     private final HrService hrService;
@@ -37,7 +37,7 @@ public class HrController {
         this.env = env;
     }
 
-// 테스트 용이므로 완료시 삭제
+    // 테스트 용이므로 완료시 삭제
     @GetMapping("/health_check")
     public String status(HttpServletRequest request) {
         return String.format("It's Working in User Service, " +
@@ -77,7 +77,7 @@ public class HrController {
         return ResponseEntity.status(HttpStatus.CREATED).body("입력 완료");
     }
 
-    @Operation(summary = "인사직원 전체 조회", description = "super 인사 관리자가 같은 회사에 속한 인사직원 모두를 리스트로 확인할 수 있다.")
+    /*@Operation(summary = "인사직원 전체 조회", description = "super 인사 관리자가 같은 회사에 속한 인사직원 모두를 리스트로 확인할 수 있다.")
     @GetMapping("/{superId}")
     public List<ResponseUser> getNormals(@PathVariable("superId") String superId){
 
@@ -89,7 +89,7 @@ public class HrController {
         });
 
         return result;
-    }
+    }*/
 
     @Operation(summary = "인사직원 상세 조회", description = "super 인사 관리자가 같은 회사에 속한 인사직원 한명의 상세정보와 담당하는 면접를 확인할 수 있다.")
     @GetMapping("/detail/{empNo}")
