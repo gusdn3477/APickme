3b2c9d5bf21dd4163da70bbae3618bdfa10ead61
박현우(Park Hyun Woo) <46596758+gusdn3477@users.noreply.github.com>
2021-11-16 17:00:12 +0900

    update

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -41,7 +41,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/**")
 //                .hasIpAddress(env.getProperty("gateway.ip"))
-                .access("hasIpAddress('172.30.144.1') or hasIpAddress('172.18.0.5') or hasIpAddress('127.0.0.1')")
+                .hasIpAddress("")
+//                .access("hasIpAddress('172.30.144.1') or hasIpAddress('172.18.0.5') or hasIpAddress('127.0.0.1')")
                 .and()
                 .addFilter(getAuthenticationFilter());
         ;
